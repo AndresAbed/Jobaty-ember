44d72d0fe5e5037c75798cf3ba51de3eec1797a3
AndresAbed <abedandres@gmail.com>
2016-02-08 16:42:11 -0300

    Renames login user model
    Corrects adapter base url

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-  host: 'https://preprojobaty.appspot.com/_ah/api/jobatyApi/v1/'
+  host: 'https://preprojobaty.appspot.com/_ah/api/jobatyApi/v1'
 });
