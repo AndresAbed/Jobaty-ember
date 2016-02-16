cba88d6b7551f8cd0b3090afa732dda6b442848a
AndresAbed <abedandres@gmail.com>
2016-02-16 17:41:30 -0300

    Removes login-user and signup-employee adapters
    Configures underscore convertion in applicatition adapter
    Removes bindAttr

--- app/adapters/signup-employee.js
+++
@@ -1,8 +0,0 @@
-import DS from 'ember-data';
-import ApplicationAdapter from './application';
-
-export default ApplicationAdapter.extend({
-  pathForType: function(type) {
-    return Ember.String.underscore(type);
-  }
-});
