8344e8280a644cb7f25b9d3d6509ec16173d7b3a
AndresAbed <abedandres@gmail.com>
2016-02-15 21:21:27 -0300

    Configures login callbacks for employees

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,13 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  loginFailed: false,
   actions: {
     login_user: function() {
+      this.setProperties({
+        loginFailed: false,
+      });
+      document.getElementById("signin-employee-input").value = "Cargando...";
       var user = this.store.createRecord('login-user', {
         email: this.get('email'),
         password: this.get('password')
       });
-      user.save();
+      user.save().then(function() {
+        //this.transitionToRoute('posts')
+        document.location = "/test";
+      }.bind(this), function(){
+        this.set("loginFailed", true);
+        document.getElementById("signin-employee-input").value = "Iniciar sesión";
+      }.bind(this));
     },
     signup_employee: function() {
       var employee = this.store.createRecord('signup-employee', {
