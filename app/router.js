0ecfad6fcb4b86904d07043f2bdffef8b8305729
AndresAbed <abedandres@gmail.com>
2016-02-25 01:56:12 -0300

    Adds companies views

--- app/router.js
+++ app/router.js
@@ -15,6 +15,15 @@ Router.map(function() {
     this.route('proposals', { path: '/propuestas' });
     this.route('rejected', { path: '/rechazadas' });
   });
+  this.route('company', {path: '/empresa'}, function() {
+    this.route('acepted-messages', { path: '/mensajes-aceptados' });
+    this.route('rejected-messages', { path: '/mensajes-rechazados' });
+    this.route('open-proposals', { path: '/propuestas-abiertas' });
+    this.route('closed-proposals', { path: '/propuestas-cerradas' });
+    this.route('new-position', { path: '/nuevo-puesto' });
+    this.route('profile', { path: '/perfil' });
+    this.route('talento', { path: '/talentos' });
+  });
 });
 
 export default Router;
