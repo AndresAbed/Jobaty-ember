d864b58948726a67099ba002d067ecb7d81f2475
AndresAbed <abedandres@gmail.com>
2016-04-06 18:52:18 -0300

    Changes employee last name from camel case yo snake case

--- app/models/signup-employee.js
+++ app/models/signup-employee.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  lastName: DS.attr('string'),
+  last_name: DS.attr('string'),
   email: DS.attr('string'),
   password: DS.attr('string')
 });
